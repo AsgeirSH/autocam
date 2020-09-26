0a00f14f69e175bb50291054fad7799da54ff741
Asgeir Stavik Hustad <askhus@online.no>
2020-09-27 01:59:17 +0200

    Updated protocol to support setting (multiple) values.

--- AutoCamCore/AutoCamCore.ino
+++ AutoCamCore/AutoCamCore.ino
@@ -20,37 +20,48 @@ previous camera, for a listening shot.
 Connect your first microphone to A0, the second to A1 and so forth.
 */
 
+#include <avr/wdt.h>
+
 //////////////////SETUP START//////////////////////////
 
-int level = 200;    // this is the trigger level to exceed from the microphones when talking in them, and the most crusial value to get right.
+int level = 200;    // This is the trigger level to exceed from the microphones when talking in them, and the most crucial value to get right.
                     // analog levels are in the range of  0-1024, normaly from 0-5v, but this can be altered by altering the analogReference
                     // in the setup section of this sketch. (https://www.arduino.cc/reference/en/language/functions/analog-io/analogreference/)
 
-int inputs =  3;    // number of michrophones to monitor. (Do NOT monitor inputs that ar not connected to an audio source, as they will report random values)
-int camstart = 1;   // input number on the video mixer of the camera corresponding to the first microphone to monitor (the rest must follow in order)
-int total = 4;      // input channel for total camera on video mixer
+int inputs =  3;    // Number of michrophones to monitor. (Do NOT monitor inputs that ar not connected to an audio source, as they will report random values)
+int camstart = 1;   // Input number on the video mixer of the camera corresponding to the first microphone to monitor (the rest must follow in order)
+int total = 4;      // Input channel for total camera on video mixer
 
 int rhythm = 1500;  // round duration i milliseconds (should be in the range of 1000-2000)
 int swing = 50;     // round duration variation in percentage (should be in the range of 10-70)
 int simultan = 30;  // simultaneous talk sensitivity before cut to total camera, in percent. 
 
 #define board 53    // number of analog pin 0 on the Arduino use 13 for Uno & Ethernet and 53 for Mega & ADK
-#define debug 1     // 1 to output debug info to the serial monitor, 0 to not
+#define debug 0     // 1 to output debug info to the serial monitor, 0 to not
 
-   //////////////////SETUP END//////////////////////////
+#define info_logging
+#ifdef info_logging
+#define info(__VA_ARGS__) Serial.println(__VA_ARGS__)
+#endif 
+
+#define debug_log
+#ifdef debug_log
+#define debugln(__VA_ARGS__) Serial.println(__VA_ARGS__);
+#endif 
+//////////////////SETUP END//////////////////////////
 
 
-int active=total;   // sets the active camera to the total camera at start
-int last=total;     // sets the last camera to the total camera at start
+int active=total;   // Sets the active camera to the total camera at start
+int last=total;     // Sets the last camera to the total camera at start
 
-int sample;         // variable that holds the current value read from the analog input
-int win;            // variable that holds the video source of the winner of the most recent round
-int count;          // variable that holds the number of samples in the most recent round  
-int rounds;         // variable that holds the number of rounds since the most recent cut
-int noone;          // variable that holds the number of rounds of consecutive silence
-int multi;          // variable that holds the number of simultaneous active inputs in the most recent round  
-int simulL;         // variable that holds the number of rounds of consecutive simultaneous active inputs
-String  reason;     // an explanation for the cut printed during debug
+int sample;         // Current value read from the analog input
+int win;            // The video source of the winner of the most recent round
+int count;          // The number of samples in the most recent round  
+int rounds;         // The number of rounds since the most recent cut
+int noone;          // The number of rounds of consecutive silence
+int multi;          // The number of simultaneous active inputs in the most recent round  
+int simulL;         // The number of rounds of consecutive simultaneous active inputs
+String  reason;     // An explanation for the cut printed during debug
 
 int back = active;   // variable that holds the previous video source, for cut backs
 long timeout;        // variable that holds end time of the current round
@@ -59,46 +70,81 @@ boolean cutBack=0;   // switch that is set to 1 if we do a cut back
 String inputString = ""; // a String to hold incoming data (config settings)
 bool stringComplete = false; // whether the string is complete
 
-void setup(){
- analogReference(INTERNAL);  // try INTERNAL for Uno and INTERNAL1V1 for Mega at line audio levels
- Serial.begin(115200);       // start the serial port to print debug messages
+void setup() {
+    analogReference(INTERNAL);  // try INTERNAL for Uno and INTERNAL1V1 for Mega at line audio levels
+    Serial.begin(115200);       // start the serial port to print debug messages
      
- inputString.reserve(32);
-     
-  if(debug){
-    Serial.println("NRKBeta AutoCam");
+    inputString.reserve(32);
+    
+    info("");
+    info("===============================");
+    info("NRKBeta AutoCam - modded by ASH");
+    info("");
     echoStatus();
-  }
- }
+}
  
 void loop(){
- ParseConfig();
- AutoCam();      
+    ParseConfig();
+    AutoCam();
 }
 
 void ParseConfig() {
-     if (stringComplete) {
-          Serial.print("Input: ");
-          Serial.println(inputString);
-          
-          if(inputString.startsWith("$INPUTS,")) {
-               inputString.remove(0,8);
-               if(inputString.toInt() != 0) {
-                    inputs = inputString.toInt();
-               }
-          }
-          else if(inputString.startsWith("$TOTAL,")) {
-               inputString.remove(0,7);
-               if(inputString.toInt() != 0) {
+    if (!stringComplete) {
+        return;
+    }
+    if(inputString.startsWith("$AC,")) {
+        inputString.remove(0,4);
+        debugln("AutoCam Command received: "+inputString);
+        // TODO: Do CRC-check if CRC is there
+        while(inputString.length() > 0) {
+            if(inputString.startsWith("NUM_INPUTS,")) {
+                inputString.remove(0,11);
+                inputs = inputString.toInt();
+                echoStatus();
+            }
+            else if(inputString.startsWith("STATUS,")) {
+                inputString.remove(0,7);
+                echoStatus();
+            }
+            else if(inputString.startsWith("FIRST_VIDEO_INPUT,")) {
+                inputString.remove(0,18);
+                if(inputString.toInt() != 0) {
+                    camstart = inputString.toInt();
+                }
+                echoStatus();
+            }
+            else if(inputString.startsWith("TOTAL,")) {
+                inputString.remove(0,7);
+                if(inputString.toInt() != 0) {
                     total = inputString.toInt();
-               }
-          }
-          // clear the string:
-          inputString = "";
-          stringComplete = false;
-          // Output new settings over serial port to verify
-          echoStatus();
-     }
+                }
+                echoStatus();
+            }
+            else if(inputString.startsWith("LEVEL,")) {
+                inputString.remove(0,7);
+                if(inputString.toInt() != 0) {
+                    level = inputString.toInt();
+                }
+                echoStatus();
+            }
+            else if(inputString.startsWith("RESETCPU")) {
+                // Enable watchdog, then infinite loop to reboot
+                wdt_enable(WDTO_30MS);
+                while(1) {};
+            }
+            else {
+                debugln("String was not empty, but no command found. Looking for next command.");
+                debugln(inputString);
+                int commaPos = inputString.indexOf(',');
+                if(commaPos != -1) {
+                    inputString.remove(0,commaPos+1);
+                } else {
+                    inputString = "";
+                    stringComplete = false;
+                }
+            }
+        }
+    }
 }
      
 void AutoCam(){
@@ -146,51 +192,54 @@ void AutoCam(){
     multi= 0;            // reset simultaneous counter
     win=0;               // reset winner
     reason = "Speaking"; // reset reason to cut
-
+    
     // loop through results for round  
     // find the input with the highest score in round
     for (int i = 1; i <=  inputs; i++) {    
-       if (input[i] > win){                 
-          win=input[i];
-          active = i + (camstart-1);
-          }   
+        if (input[i] > win){                 
+            win=input[i];
+            active = i + (camstart-1);
         }   
-   
+    }   
+    
     // log multiple inputs that are active during round
     for (int i = 1; i <=  inputs; i++) {               
        if (input[i] > simultan*(float(win)/100)){  
           multi++;  
-          }
-        } 
-
-     rounds ++;  // count round with equal result
-
-     // if the same source has been active for 4-8 rounds, do cut back to previous source
-     if(rounds > random(4,8)){
-       active = back;
-       cutBack = 1;
-       rounds = 0;
-       reason = "Cut Back";
        }
-
+    } 
+    
+    rounds ++;  // count round with equal result
+    
+    // if the same source has been active for 4-8 rounds, do cut back to previous source
+    if(rounds > random(4,8)) {
+        active = back;
+        cutBack = 1;
+        rounds = 0;
+        reason = "Cut Back";
+    }
+    
     // count rounds of simultaneous active inputs, cut to total after 2
     if (multi > 1){
         simulL++;
-        if (simulL > 2){
-          active = total;
-          reason = "Simultaneous";
+        if (simulL > 2) {
+            active = total;
+            reason = "Simultaneous";
         }
-      }
-
+    }
+    
     // check for none 
-    if(multi == 1){simulL = 0;}
-    if(multi == 0){
-      noone++;
-      if (noone == 3+(random(4))){
-          active = total;
-          reason = "Silence";
-        }  
-      }
+    if( multi == 1 ) {
+        simulL = 0;
+    }
+    
+    if( multi == 0 ) {
+        noone++;
+        if ( noone == 3+(random(4)) ) {
+            active = total;
+            reason = "Silence";
+        }
+    }
      
     // if active has changed, change source on video mixer  
     if (active != last) {
@@ -198,12 +247,12 @@ void AutoCam(){
         back=last;
         last = active;
         rounds=0;
-        }
-     }
+    }
+}
 
 void videomix(int cam){
  // send commands to your video mixer
- Serial.print("#CUT,");
+ Serial.print("$ACR,CUT,");
  Serial.println(cam);
  if(debug){
    Serial.print(" .   -    cut to camera ");
@@ -214,21 +263,23 @@ void videomix(int cam){
 }
 
 void echoStatus() {
-   Serial.print("Mic inputs: ");
+   Serial.print("$ACR,INPUTS,");
    Serial.print(inputs);
-   Serial.print("\t Wide shot on camera: ");
-   Serial.println(total);
-   Serial.print("trigger level: ");
+   Serial.print(",TOTAL,");
+   Serial.print(total);
+   Serial.print(",TRIGGER_LEVEL,");
    Serial.print(level);
-   Serial.print("\t rhythm: ");
+   Serial.print(",RHYTM,");
    Serial.print(rhythm);
-   Serial.print("ms\t swing: ");
+   Serial.print(",SWING,");
    Serial.print(swing);
-   Serial.print("%\t Simultaneous: ");
+   Serial.print(",SIMUL,");
    Serial.print(simultan);
-   Serial.println("%");
-   
-   
+   Serial.print(",LEVEL,");
+   Serial.print(level);
+   Serial.print(",FIRST_VIDEO_INPUT,");
+   Serial.print(camstart);
+   Serial.println("");
 }
 
 void serialEvent() {
